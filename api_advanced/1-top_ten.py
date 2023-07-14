0e50fd6b9ed0eec27fee1c6ecdf133d1c3ae2c19
root <d.ndungu@alustudent.com>
2023-07-14 23:37:39 +0000

    Top Ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ import requests
 def top_ten(subreddit):
     """
     Function that gets ten hot posts in a subreddit
- 
+
     :subreddit - (str) Name of subreddit
 
     :hot_list - (list) list of ten hot posts of subreddit
@@ -17,7 +17,7 @@ def top_ten(subreddit):
         return None
 
     headers = {"User-agent": "myAPI/0.0.1"}
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10"
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     res = requests.get(url, headers=headers)
     # Gets list of title of post in children
     data = res.json().get('data')
