c960bc0bffcce022d62c7f582fb3c002001586c5
root <d.ndungu@alustudent.com>
2023-07-14 23:35:54 +0000

    Top Ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ import requests
 def top_ten(subreddit):
     """
     Function that gets ten hot posts in a subreddit
-    
+ 
     :subreddit - (str) Name of subreddit
 
     :hot_list - (list) list of ten hot posts of subreddit
@@ -20,8 +20,9 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json?limit=10"
     res = requests.get(url, headers=headers)
     # Gets list of title of post in children
+    data = res.json().get('data')
     try:
-        all_posts = res.json().get('children')    
+        all_posts = data.get('children')
         for post in all_posts:
             print(post.get('data').get('title'))
     except:
