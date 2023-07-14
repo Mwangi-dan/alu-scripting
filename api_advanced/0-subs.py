307b1e6c9266d061b69c662d34c4fc3f8d86df52
root <d.ndungu@alustudent.com>
2023-07-14 22:31:31 +0000

    How many subs?

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -15,7 +15,7 @@ def number_of_subscribers(subreddit):
 
     :returns - (int) number of subscribers
     """
-    if subreddit is None or not isInstance(subreddit, str):
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
