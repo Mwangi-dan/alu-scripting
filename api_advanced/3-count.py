2abd1fbfb7cba853a93541254baf98c370c644e1
root <d.ndungu@alustudent.com>
2023-07-16 20:17:10 +0000

    Count it!

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -4,65 +4,57 @@ Module with function that counts words
 given in argument if it exists in all the titles of the
 given subreddit
 """
-from requests import get
+mport requests
 
 
-def count_words(subreddit, word_list=[], after=None, cleaned_dict=None):
+def count_words(subreddit, word_list=[], after=None, clean_dict=None):
     """
-    function that queries the Reddit API, parses the title of all hot articles,
-    and prints a sorted count of given keywords (case-insensitive, delimited by
-    spaces. Javascript should count as javascript, but java should not).
+    Function that queries subreddit for all titles and counts
+    the number of times the words in the list appear in the
+    titles.
     """
-
-    temp = []
-
-    for i in word_list:
-        temp.append(i.casefold())
-
-    cleaned_word_list = list(dict.fromkeys(temp))
-
-    if cleaned_dict is None:
-        cleaned_dict = dict.fromkeys(cleaned_word_list)
-
-    params = {'show': 'all'}
 
     if subreddit is None or not isinstance(subreddit, str):
         return None
 
-    user_agent = {'User-agent': 'myAPI/0.0.1'}
+    lowercase_list = [x.lower() for x in word_list]
+    clean_word_list = list(dict.fromkeys(lowercase_list))
 
-    url = 'https://www.reddit.com/r/{}/hot/.json?after={}'.format(subreddit,
-                                                                  after)
+    if clean_dict is None:
+        clean_dict = dict.fromkeys(clean_word_list)
 
-    response = get(url, headers=user_agent, params=params)
+    headers = {'User-Agent': 'myAPI/0.0.1'}
+    params = {'show': 'all'}
 
-    if (response.status_code != 200):
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(subreddit,
+                                                                 after)
+
+    response = requests.get(url, headers=headers, params=params)
+
+    if response:
+        all = response.json()
+        after = all.get('data').get('after')
+        raw = all.get('data').get('children')
+
+        if after is None:
+            new_dict = {k: v for k, v in clean_dict.items() if v is not None}
+
+            for k in sorted(new_dict.items(), key=lambda x: (-x[1], x[0])):
+                print('{}: {}'.format(k[0], k[1]))
+
+            return None
+        for i in raw:
+            title = i.get('data').get('title')
+            title_split = title.split()
+            title_split_lc = [x.lower() for x in title_split]
+
+            for j in title_split_lc:
+                if j in clean_dict and clean_dict[j] is None:
+                    clean_dict[j] = 1
+                elif j in clean_dict and clean_dict[j] is not None:
+                    clean_dict[j] += 1
+
+        count_words(subreddit, word_list, after, clean_dict)
+
+    else:
         return None
-
-    all_data = response.json()
-    raw1 = all_data.get('data').get('children')
-    after = all_data.get('data').get('after')
-
-    if after is None:
-        new = {k: v for k, v in cleaned_dict.items() if v is not None}
-
-        for k in sorted(new.items(), key=lambda x: (-x[1], x[0])):
-            print("{}: {}".format(k[0], k[1]))
-
-        return None
-
-    for i in raw1:
-        title = i.get('data').get('title')
-
-        split_title = title.split()
-
-        split_title2 = [i.casefold() for i in split_title]
-
-        for j in split_title2:
-            if j in cleaned_dict and cleaned_dict[j] is None:
-                cleaned_dict[j] = 1
-
-            elif j in cleaned_dict and cleaned_dict[j] is not None:
-                cleaned_dict[j] += 1
-
-    count_words(subreddit, word_list, after, cleaned_dict)
